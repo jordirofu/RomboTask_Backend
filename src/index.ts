f1526ae81c5fc3dbbae6f8a3cc42ad11b3ad0ab1
Jordi Romero <jordirofu@gmail.com>
2026-04-21 12:39:33 +0200

    Port modified to be hosted

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { app  } from "./server"
 import colors from "colors"
 
 
-const port = process.env.PORT || 4000
+const port = +(process.env.PORT || 4000)
 
-app.listen(port, ()=> {
+app.listen(port, "0.0.0.0", ()=> {
     console.log(colors.cyan.bold(`REST API funcionando en el puerto ${port}`))
 })
