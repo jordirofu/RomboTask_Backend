8705a3384c0867db950d9c07a3fb812012530d51
Jordi Romero <jordirofu@gmail.com>
2026-04-23 10:08:50 +0200

    Tem logging for cors config

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,12 +1,20 @@
 import { CorsOptions } from 'cors'
 
 export const corsConfig: CorsOptions = {
+    // origin: function (origin, callback) {
+    //     const frontendUrl = process.env.FRONTEND_URL;
+    //     if (!origin || origin === frontendUrl) {
+    //         callback(null, true);
+    //     } else {
+    //         callback(new Error('Error de CORS'));
+    //     }
+    // }
     origin: function (origin, callback) {
-        const frontendUrl = process.env.FRONTEND_URL;
-        if (!origin || origin === frontendUrl) {
-            callback(null, true);
-        } else {
-            callback(new Error('Error de CORS'));
-        }
+    console.log(`¿Es el origen "${origin}" igual a "${process.env.FRONTEND_URL}"?`)
+    if (origin === process.env.FRONTEND_URL || !origin) {
+        callback(null, true)
+    } else {
+        callback(new Error('Error de CORS'))
     }
+}
 }
